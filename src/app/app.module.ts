23083d48c43dfbe217ddff92823bbd6d6aa5b4dc
Stephanie Lakin <stephanielakin13@outlook.com>
2018-04-22 19:42:20 -0400

    Set-up & Nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {AlertModule} from 'ng2-bootstrap';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    FormsModule,
     AlertModule.forRoot()
    
   ],
